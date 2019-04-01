4a238d7c3d3591f2f3f30b436ef7d1d1cd7fe41b
github username <github email address>
2019-04-01 14:11:55 -0400

    done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
+require 'pry'
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -10,9 +12,20 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      if @@cart == []
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      resp.write add(item)
     else
       resp.write "Path Not Found"
     end
@@ -25,6 +38,15 @@ class Application
       return "#{search_term} is one of our items"
     else
       return "Couldn't find #{search_term}"
+    end
+  end
+
+  def add(item)
+    if @@items.include?(item)
+      @@cart.append(item)
+      return "added #{item}"
+    else
+      return "We don't have that item"
     end
   end
 end
